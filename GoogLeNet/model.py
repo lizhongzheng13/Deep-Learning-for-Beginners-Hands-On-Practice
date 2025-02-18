6e5863998cd2153d66dab85a6f5aa2a4c13845b2
lizhongzheng <878954714@qq.com>
2025-02-18 23:15:45 +0800

    Configure Git LFS tracking for large files

--- GoogLeNet/model.py
+++ GoogLeNet/model.py
@@ -5,3 +5,22 @@ from torch import nn
 from torchsummary import summary
 
 
+# inception的作用：增加网络深度和宽度的同时减少参数
+class Inception(nn.Module):
+    def __init__(self, in_channels, c1, c2, c3):
+        super(Inception, self).__init__()
+        self.ReLU = nn.ReLU()
+
+        # 路线1,单1*1卷积层
+        # c1卷积核的数量
+        self.p1_1 = nn.Conv2d(in_channels=in_channels, out_channels=c1, kernel_size=1)
+
+        # 路线2,1*1卷积,3*3卷积
+        # c2有两个卷积,则有两个输出
+        self.p2_1 = nn.Conv2d(in_channels=in_channels, out_channels=c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(in_channels=in_channels, out_channels=c2[1], kernel_size=3)
+
+        # 路线3,1*1卷积,5*5卷积
+        self.p3_1 = nn.Conv2d(in_channels=in_channels, out_channels=c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(in_channels=in_channels, out_channels=c3[1], kernel_size=5)
+        
